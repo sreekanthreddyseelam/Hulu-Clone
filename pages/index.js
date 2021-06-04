5629e1bf9010e287c6a0a072efb4bbd272b3eb67
SreeKanthReddy Seelam <sreekanthreddyseelam@outlook.com>
2021-06-04 19:07:30 +0530

    first commit

---
+++ pages/index.js
@@ -0,0 +1,41 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import Header from '../components/Header'
+import Nav from '../components/Nav'
+import Results from '../components/Results'
+import requests from '../utils/requests'
+
+
+
+export default function Home({results}) {
+  
+  return (
+    <div >
+      <Head>
+      <title>Hulu</title>
+      <link rel="icon" href="/favicon.ico"/>
+      </Head>
+    
+     <h1>Hello sreekanth</h1>
+     <Header />
+     <Nav />
+     <Results results={results}/>
+    </div>
+  )
+}
+
+
+export async function getServerSideProps(context){
+  const genre = context.query.genre;
+  const request =  await fetch(`https://api.themoviedb.org/3${
+    requests[genre]?.url || requests.fetchTrending.url
+  }`)
+  .then((res)=>  res.json());
+  return{
+    props:{
+      results : request.results,
+    }
+  }
+
+
+}
